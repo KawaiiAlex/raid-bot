a008db6cb50acbfc3d0e29ca111a03818446357f
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-12-02 13:34:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const chalk = require('chalk');
-const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
 const fs = require('fs');
 require('./util/eventLoader')(client);
